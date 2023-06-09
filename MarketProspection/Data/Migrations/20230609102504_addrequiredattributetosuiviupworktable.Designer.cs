303c0d3437bbaa8e9957fd56867bf75acd5e4de4
dsidali <sidali.djebline@gmail.com>
2023-06-09 11:27:34 +0100

    adjust suivi

--- MarketProspection/Data/Migrations/20230609102504_addrequiredattributetosuiviupworktable.Designer.cs
+++ MarketProspection/Data/Migrations/20230609102504_addrequiredattributetosuiviupworktable.Designer.cs
@@ -4,6 +4,7 @@ using MarketProspection.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MarketProspection.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230609102504_addrequiredattributetosuiviupworktable")]
+    partial class addrequiredattributetosuiviupworktable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MarketProspection/Models/UpworkSuivi.cs
+++ MarketProspection/Models/UpworkSuivi.cs
@@ -1,26 +1,34 @@
-﻿namespace MarketProspection.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketProspection.Models
 {
     public class UpworkSuivi
     {
         public int Id { get; set; }
 
+        [Required]
         public DateTime ProposalDate { get; set; } = DateTime.Now;
+        [Required]
         public string JobTitle { get; set; }
         public int RequiredConnects { get; set; }
         public int Bid { get; set; }
 
         public int ProjectLengthId { get; set; }
+
         public ProjectLength ProjectLength { get; set; }
 
         public int PricingTypeId { get; set; }
-        public PricingType PricingType { get; set; }
 
+        public PricingType PricingType { get; set; }
+        [Required]
         public string ClientPricing { get; set; }
         public int Pricing { get; set; }
 
         public int SubmittingProfileId { get; set; }
+
         public SubmittingProfile SubmittingProfile { get; set; }
 
+        [Required]
         public string JobLink { get; set; }
 
         public int TechnologyId { get; set; }
